0546af99a9b28f1e7cb775df76ed11469aa256d1
Olga <olga.repnitsyna@gmail.com>
2019-01-23 14:59:23 -0500

    added readme file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# GIFTastic
+
+GifTastic – A web-based Gif fetching machine.
+
+A Web based Javascript/ HTML Gif fetching application that uses the GIPHY API to fetch users choice of GIFs. GIFs have a click to play/ stop feature when clicked by user.
+
+Tech used:
+•	HTML5
+•	CSS3
+•	Bootstrap - Framework
+•	JavaScript
+•	jQuery - JavaScript Library
+•	giphy API
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -52,7 +52,7 @@ $(document).ready(function () {
         var topic = $(this).attr("data-food");
 
         // Constructing a URL to search Giphy for the chosen topic   
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=dc6zaTOxFJmzC&limit=10";
   
         // Perfoming an AJAX GET request to our queryURL
         $.ajax({
